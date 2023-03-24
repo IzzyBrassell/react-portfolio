b789258b0807e520c64dd96610ff7f3b26bc494f
Israel Brassell <israel.brassell@gmail.com>
2023-03-24 15:01:22 -0400

    chnages

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,14 +4,32 @@ import { AboutMe } from './Aboutme'
 import { Contact } from './contact'
 import { Projects } from './projects'
 import { Resume } from './resume'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+
 
 new function Layout() {
+ 
+    return(
     <html>
         <head>
-
+        <title>Izzy's Portfolio</title>
         </head>
-        <body>
-       
+        <body style={{ backgroundColor: '#42042094' }} >
+       <div>
+        <Navbar bg='secondary' expand='lg'>
+            <Navbar.Brand>Izzy Brassell</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/Aboutme">About Me</Nav.Link>
+                        <Nav.Link href="/Contact">Contact</Nav.Link>
+                        <Nav.Link href="/Projects">Projects</Nav.Link>
+                        <Nav.Link href="/Resume">Resume</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+        </Navbar>
+       </div>
        
        
         <Switch>
@@ -22,4 +40,7 @@ new function Layout() {
       </Switch>
         </body>
     </html>
+    )
 }
+
+export default Layout;
