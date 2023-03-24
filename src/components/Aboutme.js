b189ff4f031626c2263799d3e6958d2ac5646314
Israel Brassell <israel.brassell@gmail.com>
2023-03-24 12:24:43 -0400

    first commit

---
+++ src/components/Aboutme.js
@@ -0,0 +1,16 @@
+
+
+const birthdatestring = '1996-09-24'
+
+const birthdate = new Date(birthdatestring)
+
+let agediffmonths = Date.now() - birthdate.getTime()
+
+let agedate = new Date(agediffmonths)
+let age = Math.abs(agedate.getUTCFullYear() - 1970)
+
+new function aboutMe() {
+    return(
+            <></>
+    );
+}
