27cb4e450d25948ae2848ef063bd3d83d1eb10c5
Israel Brassell <israel.brassell@gmail.com>
2023-03-24 16:02:45 -0400

    :}

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,4 +1,5 @@
-
+import React from "react";
+import { Container } from "react-bootstrap";
 
 const birthdatestring = '1996-09-24'
 
@@ -11,6 +12,9 @@ let age = Math.abs(agedate.getUTCFullYear() - 1970)
 
 new function aboutMe() {
     return(
-            <></>
+        <Container fluid className="p-5 bg-dark text-white">
+        <h1 className="display-4">About Me</h1>
+        <p className="lead">This is a simple Jumbotron component for React Bootstrap.</p>
+      </Container>
     );
 }
