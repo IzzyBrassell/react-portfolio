e0a6f2f637b44c8b60f986e00f941f7cf4039498
Israel Brassell <israel.brassell@gmail.com>
2023-03-27 19:57:18 -0400

    continuing bug fixes

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { AboutMe } from './Aboutme'
+import { AboutMe } from './Aboutme';
 import { Contact } from './contact'
 import { Projects } from './projects'
 import { Resume } from './resume'
